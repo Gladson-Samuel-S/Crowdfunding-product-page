4703fba63b7659cee335a1862aa55a313b62b8c4
Gladson Samuel S <74543240+Gladson-Samuel-S@users.noreply.github.com>
2021-05-28 14:35:21 +0530

    initial commit :tada:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const Atag = styled.a`
+  background-color:  grey;
+  font-size: 40px;
+`
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
+      <br />
+      <Atag href="#">
+        Let's go styled with styled components
+      </Atag>
     </div>
-  );
+  );                               
 }
 
 export default App;
